e41d55ebf6c90f15839464538edeeccf258101bc
Tamim khan <mdtamimkhan842@gmail.com>
2025-04-14 02:35:46 +0600

    added method and update array

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,26 +1,84 @@
 ﻿using System;
-class Test {
+using System.Linq;
+class Test
+{
 
-  public static void Main(string[] args) {
-       
-      //  array declear korar jonno first of all datatype declear kora lage then size declear kora lage then variable declear kora lage
-      string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+  public static void Main(string[] args)
+  {
 
-      for(int i=0;i<(int)cars.Length;i++)Console.WriteLine(cars[i]);
-      Console.WriteLine();
-      // change the array element
-      cars[0] = "Opel";
-      // now print the array element
-      for(int i=0;i<(int)cars.Length;i++)Console.WriteLine(cars[i]);
-       Console.WriteLine();
+    //  array declear korar jonno first of all datatype declear kora lage then size declear kora lage then variable declear kora lage
+    string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
 
-      int[] myNum = {10, 20, 30, 40};
-      for(int i=0;i<(int)myNum.Length;i++)Console.WriteLine(myNum[i]);
-      Console.WriteLine();
+    for (int i = 0; i < (int)cars.Length; i++) Console.WriteLine(cars[i]);
+    Console.WriteLine();
+    // change the array element
+    cars[0] = "Opel";
+    // now print the array element
+    for (int i = 0; i < (int)cars.Length; i++) Console.WriteLine(cars[i]);
+    Console.WriteLine();
 
-// others way string array declear
-   
-      string[] carss = new string[] {"Volvo", "BMW", "Ford"};
-      for(int i=0;i<(int)carss.Length;i++)Console.WriteLine(carss[i]);
+    int[] myNum = { 10, 20, 30, 40 };
+    for (int i = 0; i < (int)myNum.Length; i++) Console.WriteLine(myNum[i]);
+    Console.WriteLine();
+
+    // others way string array declear
+
+    string[] carss = new string[] { "Volvo", "BMW", "Ford" };
+    for (int i = 0; i < (int)carss.Length; i++) Console.WriteLine(carss[i]);
+    Console.WriteLine();
+
+    string[] garis = { "Volvo", "BMW", "Ford", "Mazda" };
+
+    foreach (string gari in garis)
+      Console.WriteLine(gari);
+
+    // string array sort
+    Console.WriteLine();
+    Array.Sort(garis);
+
+    foreach (string gari in garis)
+      Console.WriteLine(gari);
+
+    // int n = Convert.ToInt32(Console.ReadLine());
+    // int[] array = new int[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //   int x = Convert.ToInt32(Console.ReadLine());
+    //   array[i] = x;
+    // }
+    // Array.Sort(array);
+
+    // foreach (int arr in array) Console.WriteLine(arr);
+
+    int[] myNumbers = { 5, 1, 8, 9 };
+    Console.WriteLine(myNumbers.Max());
+    Console.WriteLine(myNumbers.Min());
+    Console.WriteLine(myNumbers.Sum());
+
+    int x = 10, y = 20;
+    int mx = Math.Min(x, y);
+    Console.WriteLine(mx);
+    Console.WriteLine();
+
+
+
+    // multidimensional array
+    // Good to know: The single comma[,] specifies that the array is two - dimensional.A three - dimensional array would have two commas: int[,,].
+    // Remember that: Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
+    int[,] number = { { 1, 4, 2 }, { 1, 8, 5 } };
+    number[0, 0] = 5;  // Change value to 5 into the (0,0) index
+                       // Console.WriteLine(number[1, 1]);
+                       // print of the 2d array using foreach loop 
+    foreach (int val in number) Console.Write($"{val} ");
+    Console.WriteLine();
+
+
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+      for (int j = 0; j < number.GetLength(1); j++)
+        Console.Write($"{number[i, j]} ");
+    }
+    Console.WriteLine();
+
   }
 }
---
+++ Method/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Test
+{
+    static void Mymethod()
+    {
+        Console.WriteLine("Calling my method");
+    }
+
+    static void passingmethod(string s)
+    {
+        Console.WriteLine(s);
+    }
+
+    static void multiplepass(string name, int age, string universityName)
+    {
+        Console.WriteLine($"My name is {name}, age is {age} and my university name is {universityName}, i'm study here already 4 yeras. ");
+    }
+
+    static void defaultparameter(string name = "Tamim khan")
+    {
+        Console.WriteLine(name);
+    }
+
+    static int factorial(int n)
+    {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) fact *= i;
+        return fact;
+    }
+
+
+    static void Named_Arguments(string child1, string child2, string child3)
+    {
+        Console.WriteLine("The youngest child is: " + child3);
+    }
+
+    public static void Main(string[] args)
+    {
+        Mymethod();
+
+        // method pass parameter
+        passingmethod("Tamim khan");
+
+        // multiple passing parameter 
+        multiplepass("Tamim khan", 24, "Prime university");
+
+        defaultparameter("Mannan khan");
+        defaultparameter();
+
+        int fact = factorial(5);
+        Console.WriteLine(fact);
+
+        Named_Arguments(child1: "hamim", child2: "khata", child3: "Bala");
+
+    }
+
+
+
+}
